4955680c2064be375f0c886f0884e8caaa6a27bc
Lav-Venkat <inukurthivenkatesh@gmail.com>
2022-09-20 13:11:28 +0530

    Added sed in Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -3,20 +3,20 @@ LOG_FILE=/tmp/mongoDB.log
 
 
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> /tmp/mongoDB.log
-echo "Status $?0"
+echo "Status $?"
 yum install -y mongodb-org &>> /tmp/mongoDB.log
-echo "Status $?0"
+echo "Status $?"
 systemctl enable mongod &>> /tmp/mongoDB.log
 systemctl start mongod &>> /tmp/mongoDB.log
-echo "Status $?0"
-sed 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf # Need to verify if the script doesn't work
+echo "Status $?"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf # Need to verify if the script doesn't work
 
 systemctl restart mongod &>> /tmp/mongoDB.log
-echo "Status $?0"
+echo "Status $?"
 sudo curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 sudo cd /tmp
 sudo unzip mongodb.zip
 sudo cd mongodb-main
 mongod<catalogue.js
 mongod<users.js
-echo "Status $?0"
+echo "Status $?"
