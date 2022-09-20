a57a4d0ba3ca367bf961550e475fd11e433a2f33
Lav-Venkat <inukurthivenkatesh@gmail.com>
2022-09-20 12:38:32 +0530

    Added variables in frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/bash
+LOG_FILE=/tmp/frontend.log
 echo "Installing Nginx"
 yum install nginx -y
 
 echo "downloading Nginx web content"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> $LOG_FILE
+systemctl start nginx &>> $LOG_FILE
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> /tmp/frontend.log
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $LOG_FILE
 
 
 cd /usr/share/nginx/html
 
 echo "removing unwanted files"
 
-rm -rf * &>> /tmp/frontend.log
+rm -rf * &>> $LOG_FILE
 
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
+unzip /tmp/frontend.zip &>> $LOG_FILE
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl restart nginx &>> /tmp/frontend.log
+systemctl restart nginx &>> $LOG_FILE
