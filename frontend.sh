fdc81ce99dc2a867152bfa7ed6a8e824e2d7ec2a
Lav-Venkat <inukurthivenkatesh@gmail.com>
2022-09-20 19:20:24 +0530

    Added common function to maintain common code

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/bash
 LOG_FILE=/tmp/frontend.log
+
+source common.sh
+
 echo "Installing Nginx"
 yum install nginx -y &>> $LOG_FILE
-
+StatusCheck $?
 echo "downloading Nginx web content"
+# shellcheck disable=SC2129
 systemctl enable nginx &>> $LOG_FILE
+StatusCheck $?
 systemctl start nginx &>> $LOG_FILE
+StatusCheck $?
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $LOG_FILE
-
+StatusCheck $?
 
 cd /usr/share/nginx/html
 
@@ -17,7 +23,9 @@ echo "removing unwanted files"
 rm -rf * &>> $LOG_FILE
 
 unzip /tmp/frontend.zip &>> $LOG_FILE
+StatusCheck $?
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-
+StatusCheck $?
 systemctl restart nginx &>> $LOG_FILE
+StatusCheck $?
--- mongodb.sh
+++ mongodb.sh
@@ -1,28 +1,27 @@
 echo "*****Setting up Mongo DB*****"
 LOG_FILE=/tmp/mongoDB.log
-
+source common.sh
 
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> /tmp/mongoDB.log
-echo "Status $?"
+StatusCheck $?
 yum install -y mongodb-org &>> /tmp/mongoDB.log
-echo "Status $?"
+StatusCheck $?
 systemctl enable mongod
 systemctl start mongod
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf # Need to verify if the script doesn't work
 echo "Status $?"
 systemctl restart mongod
-echo "Status $?"
-
+StatusCheck $?
 echo"*****Download mongodb schema*****"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 echo "*****Extract schema*****"
 cd /tmp
 unzip mongodb.zip
 cd mongodb-main
-
+StatusCheck $?
 
 echo "*****load catalogue service schema*****"
 mongo < catalogue.js
 mongo < users.js
-echo "Status $?"
+StatusCheck $?
